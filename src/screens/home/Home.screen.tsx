02625137e9b98dad3468aaf78f56e6aa6abed98d
Soyoon Choi <soyoon.choi@code.berlin>
2021-10-15 13:17:08 +0200

    set up eslint

--- src/screens/home/Home.screen.tsx
+++ src/screens/home/Home.screen.tsx
@@ -4,35 +4,35 @@ import { TelemetryItemProps, ResponseTelemetryDataProps } from '../../types';
 import '../../App.css';
 
 const Home = () => {
-    const [telemetryData, setTelemetryData] = useState<TelemetryItemProps[]>([]);
-    const fetchData = async () => {
-      try {
-        const response = await appApiClient.get<ResponseTelemetryDataProps>('/');
-        setTelemetryData(response.data.telemetryData);
-      } catch (e) {
-        console.error(e);
-      }
-    };
-    
-    useEffect(() => {
-      fetchData();
-    }, []);
+  const [telemetryData, setTelemetryData] = useState<TelemetryItemProps[]>([]);
+  const fetchData = async () => {
+    try {
+      const response = await appApiClient.get<ResponseTelemetryDataProps>('/');
+      setTelemetryData(response.data.telemetryData);
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
-    return (
-        <div className='App'>
-        {telemetryData &&
-          telemetryData.map((telemetryItem: TelemetryItemProps) => (
-            <>
-              <div>date: {telemetryItem.eventTimestamp}</div>
-              <div>sitting time: {telemetryItem.sittingTime}</div>
-              <div>temperature: {telemetryItem.temperature}</div>
-              <div>humidity: {telemetryItem.humidity}</div>
-              <div>dust concentration: {telemetryItem.dustConcentration}</div>
-              <br />
-            </>
-          ))}
-      </div>
-    )
-}
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className='App'>
+      {telemetryData &&
+        telemetryData.map((telemetryItem: TelemetryItemProps) => (
+          <>
+            <div>date: {telemetryItem.eventTimestamp}</div>
+            <div>sitting time: {telemetryItem.sittingTime}</div>
+            <div>temperature: {telemetryItem.temperature}</div>
+            <div>humidity: {telemetryItem.humidity}</div>
+            <div>dust concentration: {telemetryItem.dustConcentration}</div>
+            <br />
+          </>
+        ))}
+    </div>
+  );
+};
 
 export default Home;
