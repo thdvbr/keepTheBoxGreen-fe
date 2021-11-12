1a037b2d3117e2c35add9da63dd37645e0925fe7
Soyoon Choi <soyoon.choi@code.berlin>
2021-11-12 11:37:33 +0100

    change baseURL and typescript config

--- src/api/appApiClient.ts
+++ src/api/appApiClient.ts
@@ -1,7 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://keeptheboxgreen-api.azurewebsites.net/',
+  baseURL: 'https://keeptheboxgreen-prod.azurewebsites.net/',
   headers: {
     'Content-type': 'application/json',
   },
