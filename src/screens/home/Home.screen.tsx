9fcfad6b2db90ebb0470ef9094795893430dc6f5
thdvbr <soyoon.choi@code.berlin>
2021-11-19 21:34:26 +0100

    render current data

--- src/screens/home/Home.screen.tsx
+++ src/screens/home/Home.screen.tsx
@@ -1,10 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import React, { useState, useEffect } from 'react';
 import appApiClient from '../../api/appApiClient';
 import { TelemetryItemProps, ResponseTelemetryDataProps } from '../../types';
 import '../../App.css';
 
 const Home = () => {
-  const [telemetryData, setTelemetryData] = useState<TelemetryItemProps[]>([]);
+  const [telemetryData, setTelemetryData] = useState<TelemetryItemProps>([]);
   const fetchData = async () => {
     try {
       const response = await appApiClient.get<ResponseTelemetryDataProps>('/');
@@ -20,22 +22,26 @@ const Home = () => {
 
   return (
     <div className='App'>
+      Current Data:
       {telemetryData &&
-        telemetryData.map((telemetryItem: TelemetryItemProps) => (
-          <div key={telemetryItem.id}>
-            <div>
-              date: {telemetryItem.EventEnqueuedUtcTime.substring(0, 10)}
-            </div>
-            <div>
-              time: {telemetryItem.EventEnqueuedUtcTime.substring(11, 19)}
-            </div>
-            <div>sitting time: {telemetryItem.sittingTime}</div>
-            <div>temperature: {telemetryItem.temperature}</div>
-            <div>humidity: {telemetryItem.humidity}</div>
-            <div>dust concentration: {telemetryItem.dustConcentration}</div>
-            <br />
-          </div>
-        ))}
+        telemetryData.map(
+          (telemetryItem: TelemetryItemProps, index, array) =>
+            array.length - 1 === index && (
+              <div key={index}>
+                <div>
+                  date: {telemetryItem.EventEnqueuedUtcTime.substring(0, 10)}
+                </div>
+                <div>
+                  time: {telemetryItem.EventEnqueuedUtcTime.substring(11, 19)}
+                </div>
+                <div>sitting time: {telemetryItem.sittingTime}</div>
+                <div>temperature: {telemetryItem.temperature}</div>
+                <div>humidity: {telemetryItem.humidity}</div>
+                <div>dust concentration: {telemetryItem.dustConcentration}</div>
+                <br />
+              </div>
+            )
+        )}
     </div>
   );
 };
