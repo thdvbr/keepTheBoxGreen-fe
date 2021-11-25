d73bd2d104913e84252c971da76ba6a9d897095a
Jongwoo Park <jongwoo.park@code.berlin>
2021-11-25 17:05:32 +0100

    p5js imported

---
+++ public/sketch.js
@@ -0,0 +1,43 @@
+var t = 0
+function RainDrop(l, speed) {
+  this.l = l
+  this.speed = speed
+  this.x = random(0, width)
+  this.y = random(0, height)
+  this.update = function () {
+    this.y += this.speed
+    this.x += noise(t)
+    if (this.y >= height) {
+      this.y = 0
+    }
+    if (this.x >= width) {
+      this.x = 0
+    }
+  }
+  this.show = function () {
+    line(this.x, this.y, this.x, this.y - l)
+  }
+}
+
+function setup() {
+  createCanvas(window.innerWidth / 1.05, window.innerHeight / 2)
+  layers = []
+  for (i = 0; i < 7; i++) {
+    layers[i] = []
+    for (ii = 0; ii < 300; ii++) {
+      rd = new RainDrop(2 * i, i + 1)
+      layers[i].push(rd)
+    }
+  }
+  t += 0.001
+}
+
+function draw() {
+  background("#38A169")
+  for (i = 0; i < layers.length; i++) {
+    for (ii = 0; ii < layers[i].length; ii++) {
+      layers[i][ii].update()
+      layers[i][ii].show()
+    }
+  }
+}
--- src/components/p5_box.tsx
+++ src/components/p5_box.tsx
@@ -1,72 +1,7 @@
 //@ts-nocheck
 
 import { Box, Center } from '@chakra-ui/react';
-import { ReactP5Wrapper } from "react-p5-wrapper";
-
-function sketch(p5) {
-  const width = window.innerWidth / 1.05;
-  const height = window.innerHeight / 2.2;
-
-  const random = (max) => {
-    return Math.floor(Math.random() * max);
-  }
-
-  class Ball {
-    constructor(x, y, size, speedX, speedY, dirX, dirY, weight, color) {
-      this.x = x;
-      this.y = y;
-      this.size = size;
-      this.speedX = speedX;
-      this.speedY = speedY;
-      this.dirX = dirX;
-      this.dirY = dirY;
-      this.weight = weight;
-      this.color = (random(255), random(255), random(255));
-    }
-  }
-
-  const ballArr = [];
-  const ballNumber = 500;
-
-  const createBall = () => {
-    return new Ball(
-      random(width),  // x
-      random(height), // y
-      random(10),      // size
-      random(1, 5),   // speedX
-      random(1, 5),   // speedY
-      random(-1, 1),  // dirX
-      random(-1, 1),  // dirY
-      random(1, 5),   // weight
-    );
-  }
-
-  for (let i = 0; i < ballNumber; i++) {
-    ballArr.push(createBall());
-  }
-
-  console.log(ballArr);
-
-  function ball() {
-    p5.circle(0, 0, 10);
-    p5.noStroke();
-    for (let i = 0; i < ballNumber; i++) {
-      p5.circle(ballArr[i].x, ballArr[i].y, ballArr[i].size);
-    }
-  }
-
-  p5.setup = () => {
-    p5.createCanvas(width, height, p5.WEBGL);
-  }
-  p5.draw = () => {
-    p5.background("#38A169");
-    p5.translate(- width / 2, - height / 2);
-    ball();
-    ball();
-    ball();
-  };
-
-}
+import ScriptTag from 'react-script-tag';
 
 export default function P5Box() {
   return (
@@ -74,7 +9,7 @@ export default function P5Box() {
       <Box>some small title</Box>
       <Box>
         <Center borderRadius='10px'>
-          <ReactP5Wrapper sketch={sketch} />
+          <ScriptTag type="text/javascript" src="sketch.js" />
         </Center>
       </Box>
     </>
